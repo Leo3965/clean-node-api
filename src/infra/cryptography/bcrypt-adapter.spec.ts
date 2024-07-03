811386467c45d2be7865e7adea3a65f707736609
Leonardo Freitas <leofreitas.engineer@gmail.com>
2024-07-02 21:11:55 -0300

    refactor: add mock do bcrypt library

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -1,11 +1,25 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
+jest.mock('bcrypt', () => ({
+  async hash(): Promise<string> {
+    return new Promise((resolve) => {
+      resolve('hash')
+    })
+  }
+}))
+
 describe('BCrypt Cryptography', () => {
   test('Should call bcrypt with correct value', async () => {
     const sut = new BcryptAdapter()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any value')
     expect(hashSpy).toHaveBeenCalledWith('any value', 12)
+  })
+
+  test('Should return hash on success', async () => {
+    const sut = new BcryptAdapter()
+    const hash = await sut.encrypt('any value')
+    expect(hash).toBe('hash')
   })
 })
