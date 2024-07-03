33873e222377e93525f0d36cda32f2b69ea48e0b
Leonardo Freitas <leofreitas.engineer@gmail.com>
2024-07-03 12:01:06 -0300

    feat: add express

---
+++ src/main/server.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const PORT = process.env.PORT || 8080
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
