cddc3aca458d7a6cacf8a38a14fb2975a4098cd6
Leonardo Freitas <leofreitas.engineer@gmail.com>
2024-07-03 16:28:23 -0300

    feat: add signup factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,13 @@
+import { SignUpController } from '../../presentation/controllers/signup'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
+import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository'
+import { BcryptAdapter } from '../../infra/cryptography/bcrypt-adapter'
+
+export function SignUpFactory(): SignUpController {
+  const bcrypt = new BcryptAdapter()
+  const repository = new AccountMongoRepository()
+  const useCase = new DbAddAccount(bcrypt, repository)
+  const emailValidator = new EmailValidatorAdapter()
+  return new SignUpController(emailValidator, useCase)
+}
